4fcb32134cfe96bd8e0eec1091374ea3a705df9b
Looknoeyes NaKaMa <mnkalbete@gmail.com>
2021-04-03 14:19:34 -0700

    mongoose, routes and controllers

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-const express = require("express")
-const app = express()
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+// import routes
+const userRoutes = require('./routes/user');
 
+
+// app
+const app = express();
+
+// db
 mongoose.connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true }
-).then(() => console.log('DB Connected'))
+    process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+}).then(() => console.log('DB Connected'));
 
 mongoose.connection.on('error', err => {
     console.log(`DB connection error: ${err.message}`)
-})
-    
+});
 
-
-app.get('/', (req, res) => {
-    res.send("hello from node")
-}); 
+// routes middleware
+app.use("/api",userRoutes);
 
 const port = process.env.PORT || 3000
 
