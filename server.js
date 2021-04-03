ef6aa7f9db36ed0be47556824d1ff27f7b38133f
Looknoeyes NaKaMa <mnkalbete@gmail.com>
2021-04-03 13:43:02 -0700

    mongoose package and updated server.js information

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express")
 const app = express()
-require('dotenv').config()
 const gi = require(`.gitignore`);
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+mongoose.connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true }
+).then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+})
+    
+
 
 app.get('/', (req, res) => {
     res.send("hello from node")
