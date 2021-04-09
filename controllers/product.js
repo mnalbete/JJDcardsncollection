0c5fbc23c553033658e5726a3d1bba3a0ba0abae
Looknoeyes NaKaMa <mnkalbete@gmail.com>
2021-04-08 22:51:09 -0700

    products.js code

--- controllers/product.js
+++ controllers/product.js
@@ -3,29 +3,49 @@ const _ = require("lodash");
 const Product = require("../models/product");
 const fs = require("fs");
 const { errorHandler } = require("../helpers/dbErrorHandler");
-exports.create = (req, res) => {
-    let form = new formidable.IncomingForm()
-    form.keepExtensions = true
-    form.parse(req, (err, fields, files) => {
-        if(err) {
-            return res.status(400).json({
-                error: "Image could not be uploaded"
-            })
-        }
-        let product = new Product(fields)
 
-        if(files.photo) {
-            product.photo.data = fs.readFileSync(files.photo.path)
-            product.photo.contentType = files.photo.type
+exports.create = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            });
+        }
+        // check for all fields
+        const { name, description, price, category, quantity, shipping } = fields;
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields are required'
+            });
+        }
+
+        let product = new Product(fields);
+
+        // 1kb = 1000
+        // 1mb = 1000000
+
+        if (files.photo) {
+            // console.log("FILES PHOTO: ", files.photo);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size'
+                });
+            }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
         }
 
         product.save((err, result) => {
-            if(err) {
+            if (err) {
+                console.log('PRODUCT CREATE ERROR ', err);
                 return res.status(400).json({
-                    error: errorHandler(error)
+                    error: errorHandler(err)
                 });
             }
             res.json(result);
-        })
-    })
+        });
+    });
 };
