52221f2a7be4ae2b797f8536e7d8ced67a51f865
Looknoeyes NaKaMa <mnkalbete@gmail.com>
2021-04-08 23:52:02 -0700

    controller updates and routes updates

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,18 @@
 const Category = require("../models/category");
 const { errorHandler } = require("../helpers/dbErrorHandler");
+const { errors } = require("formidable");
+
+exports.categoryById = (req, res, next) => {
+    categoryById.findById(id).exec((err, category) => {
+        if (err || !category) {
+            return res.status(400).json({
+                error: "Category does not exist"
+            });
+        }
+        req.category = category;
+        next();
+    });
+}
 
 exports.create = (req, res) => {
     const category = new Category(req.body)
@@ -11,4 +24,48 @@ exports.create = (req, res) => {
         }
         res.json({data});
     });
+};
+
+
+exports.read = (req, res) => {
+    return res.json(req.category);
+};
+
+exports.update = (req, res) => {
+    const category = req.category
+    // add more to the update section
+    category.name = req.body.name
+    category.save((err, data) => {
+        if(err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data);
+    })
+};
+
+exports.remove = (req, res) => {
+    const category = req.category
+    category.remove((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: "Category deleted"
+        });
+    })
+};
+
+exports.list = (req, res) => {
+    Category.find().exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data);
+    })
 };
