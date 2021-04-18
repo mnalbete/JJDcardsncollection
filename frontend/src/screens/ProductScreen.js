6e56a7271c30c56a7293c471409c1314219c2dbf
Looknoeyes NaKaMa <mnkalbete@gmail.com>
2021-04-17 19:19:07 -0700

    update

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+    const product = products.find((p) => p._id === match.params.id)
+
     return (
-        <div>
-            Product
-        </div>
+        <>
+           <Link className= 'btn btn-dark my-3' to= '/'>Go Back</Link>
+           <Row>
+               <Col md = {6}>
+                   <Image src = {product.image} alt= {product.name}></Image>
+               </Col>
+               <Col md= {3}></Col>
+           </Row>
+        </>
     )
 }
 
