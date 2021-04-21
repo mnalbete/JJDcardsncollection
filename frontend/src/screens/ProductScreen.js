61775b15d372a603b495344f024b723aa0c16691
Looknoeyes NaKaMa <mnkalbete@gmail.com>
2021-04-20 21:38:43 -0700

    server

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -30,7 +30,7 @@ const ProductScreen = ({ match }) => {
                             Price: ${product.price}
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            Description: ${product.description}
+                            Description: {product.description}
                         </ListGroup.Item>
                     </ListGroup>
                 </Col>
